8a8fbd12e8518d89fc20ea7281b4cb0c324122ac
Roger Rodrigues de Oliveira <roger.r.oliveira@unesp.br>
2022-10-27 22:38:48 -0300

    Model teste para rota 'Sobre'

---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App\Model\Entity;
+
+    class Organization {
+        public $id = 1;
+        public $name = "Satulg";
+        public $site = "satulg@hotmail.com";
+        public $description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam temporibus, ullam odio dolorum sunt accusantium magnam! Dignissimos error sint amet impedit voluptatibus illo sequi ad dolores harum ea, nesciunt a!";
+    }
+?>
