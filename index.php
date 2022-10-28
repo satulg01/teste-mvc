e43a536b33cd235a84ecda76c0b75113b77cf551
Roger Rodrigues de Oliveira <roger.r.oliveira@unesp.br>
2022-10-27 22:38:05 -0300

    Modularização das rotas

--- index.php
+++ index.php
@@ -1,93 +1,32 @@
 <?php
 	require __DIR__ . "/vendor/autoload.php";
 
-	define("URL", "http://localhost");
+	use \App\Http\Router;
+	use \App\Utils\View\View;
 
-	use \App\Controller\Pages\Home;
-	// use \App\Http\Router;
+	define("URL", "http://localhost/teste-mvc");
 
-	echo Home::getHome();
+	#Define o valor padrão das variáveis
+	View::init([
+		'URL' => URL,
+	]);
 
-	session_start();
 
-	exit;
-
-	/*
+	#Inicializa o roteador
 	$obRouter 	= new Router(URL);
-	$connection = new Connection("localhost", "servidor", "root", "");
-	$db 		= $connection->returnConnection();
-	$relation 	= new Relation($db);
-	*/
+
+	#Inclui as rotas
+	include __DIR__ . "/routes/pages.php";
+
+	
+	// $connection = new Connection("localhost", "servidor", "root", "");
+	// $db 		= $connection->returnConnection();
+	// $relation 	= new Relation($db);
+	
+
+
+
+	$obRouter->run()
+				->sendResponse();
 
 ?>
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="./_css/style.css">
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-		<script src="_js/jquery-3.6.0.min.js"></script>
-		<!-- <script src="https://kit.fontawesome.com/6cfdfed1de.js" crossorigin="anonymous"></script> -->
-		<script src="_js/jquery.mask.js"></script>
-		<link rel="icon" type="image/x-icon" href="./_img/favicon.png">
-		<script src="_js/funcoes.js"></script>
-		<title>Satulg</title>
-	</head>
-	<body>
-		<div id="line-1">
-			<div class="logo"><img src="./_img/logo/logo.png"></div>
-			<div class="cliente"></div>
-		</div>
-		<div id="line-2">
-			<div class="header-menu"></div>
-			<?php
-
-				$obRouter->get("/", [
-					function(){
-						return new Response(200, "Olá mundo");
-					}
-				]);
-
-				$obRouter->run()
-						 ->sendResponse();
-
-
-
-				exit();
-
-				
-				
-				
-				
-				
-				
-				
-				$links = $_GET["url"];
-				$links = explode("/", $links);
-
-				if(isset($links[0])) {
-					$controller = $links[0];
-					array_shift($links);
-
-				}
-				print_r($links);
-				exit();
-
-			?>
-		</div>
-	</body>
-</html>
-<script type="text/javascript">
-	$("title").html("Satulg - <?php echo $pagina ?>");
-	$(document).ready(function(){
-  		$('.format-data').mask('00/00/0000');
-  		$('.format-hora').mask('00:00:00');
-  		$('.format-data-hora').mask('00/00/0000 00:00:00');
-  		$('.format-cep').mask('00000-000');
-  		$('.format-celular').mask('(00) 0000-0000');
-  		$('.format-cpf').mask('000.000.000-00', {reverse: true});
-  		$('.format-cnpj').mask('00.000.000/0000-00', {reverse: true});
-  		$('.format-real').mask('000.000.000.000.000,00', {reverse: true});
-  	});
-</script>
